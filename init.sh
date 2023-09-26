924f2d2db0777037998fd7a0c53210d47bc3d726
Jørgen Kvalsvik <j@lambda.is>
2023-09-26 22:32:05 +0900

    Add init.sh, initial notes, certificates

---
+++ init.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function help {
+    echo "usage: init.sh [--user user] mattermost.softwareunderground.org"
+    echo "Please read the README for more details"
+}
+
+# Default user to whoami, so that the letsencrypt are decrypted by default with
+# the user that is running the script. If the crypt/name-* does not match the
+# current user you can override it with --user.
+user=$(whoami)
+argv=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --help)
+            help
+            exit 0
+            ;;
+        --user)
+            user="$2"
+            shift
+            ;;
+        *)
+            argv+=("$1")
+            ;;
+    esac
+    shift
+done
+
+host="${argv[0]}"
+
+users=(j)
+
+debpkgs=(
+    sudo
+    fail2ban
+    ufw
+    stow
+    rsync
+    postgresql
+    postgresql-common
+    nginx
+    certbot
+    python3-certbot-nginx
+)
+
+mattermost_version=9.0.0
+mattermost_tarball=mattermost-${mattermost_version}-linux-amd64.tar.gz
+mattermost_url=https://releases.mattermost.com/${mattermost_version}/${mattermost_tarball}
+
+set -x
+
+ssh root@${host} "bash - " << EOF
+    set -x
+    apt-get update
+    apt upgrade -y
+    apt-get install -y ${debpkgs[@]}
+
+    for user in ${users[@]}; do
+        if ! id \$user 2>/dev/null; then
+            # create the user with disabled password (no prompt),
+            # delete it to make it empty, then expire so users must set it on first
+            # login.
+            #
+            # This is to make sudo require the extra password check which is a nice
+            # extra mechanism to prevent accidental changes.
+            adduser --disabled-password --gecos "" \$user
+            passwd -d \$user
+            chage -d 0 \$user
+        fi
+    done
+
+    for user in ${users[@]}; do
+        adduser \$user sudo
+    done
+
+    useradd --system --create-home --user-group mattermost
+    mkdir -p /stow
+EOF
+
+for user in ${users[@]}; do
+    if [ -d home/$user ]; then
+        rsync -av --chown=$user:$user home/$user root@${host}:/stow/home
+    fi
+done
+
+rsync -av etc lib root@${host}:/stow
+rsync -av --chown=mattermost:mattermost --chmod=g+w opt/mattermost-* root@${host}:/stow/opt
+gpg --decrypt cert/j-letsencrypt.tar.gz.gpg | ssh root@${host} "tar xzf - -C /stow/etc"
+
+set +x
+mmuserpw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 25)
+set -x
+
+ssh -q root@${host} "bash -" << EOF
+    if ! su - postgres --command \
+        "psql postgres -tXAc \"SELECT 1 FROM pg_roles WHERE rolname='mmuser'\"" \
+        | grep -q 1; then
+
+        su - postgres --command \
+            "psql -c \"CREATE USER mmuser WITH PASSWORD '${mmuserpw}';\""
+
+        echo "Writing mattermost .pgpass"
+        echo "*:*:mattermost:mmuser:${mmuserpw}" > /home/mattermost/.pgpass
+        chmod 0600 /home/mattermost/.pgpass
+        chown mattermost:mattermost /home/mattermost/.pgpass
+    fi
+
+    if ! su - postgres --command "psql -lq" | grep -qw mattermost; then
+        su - postgres --command "psql --file -" << END
+            CREATE DATABASE mattermost;
+            GRANT ALL ON DATABASE mattermost TO mmuser;
+            ALTER DATABASE mattermost OWNER TO mmuser;
+            GRANT USAGE, CREATE ON SCHEMA PUBLIC TO mmuser;
+END
+    fi
+EOF
+
+ssh -q root@${host} "bash -" << EOF
+    # disable all sites but mattermost
+    rm /etc/nginx/sites-*/*
+
+    if [[ ! -f /tmp/${mattermost_tarball} ]]; then
+        set -euo pipefail
+        wget ${mattermost_url} --no-verbose -O /tmp/${mattermost_tarball}
+        mkdir -p /opt/mattermost-${mattermost_version}
+        tar xf /tmp/${mattermost_tarball} -C /opt/mattermost-${mattermost_version} \
+            --strip-components 1 \
+            --exclude=mattermost/config/config.json
+        chown -R mattermost:mattermost /opt/mattermost-${mattermost_version}
+        chmod g+w -R /opt/mattermost-${mattermost_version}
+    fi
+
+    mkdir -p /etc/letsencrypt
+    # cli.ini is written by the bot package, but we bring our own
+    rm -f /etc/letsencrypt/cli.ini
+
+    set -xeuo pipefail
+    for u in ${users[@]}; do chown -R \$u:\$u /stow/home/\$u; done
+    for u in ${users[@]}; do stow -d /stow/home -t /home/\$u \$u; done
+    stow -d /stow/etc -t /etc/ssh ssh
+    stow -d /stow/etc -t /etc/fail2ban fail2ban
+    stow -d /stow/etc -t /etc/nginx nginx
+    stow -d /stow/etc -t /etc/letsencrypt letsencrypt
+    stow -d /stow/lib -t /lib/systemd systemd
+    stow -d /stow/opt -t /opt/mattermost-${mattermost_version} mattermost-${mattermost_version}
+
+    systemctl enable fail2ban
+    systemctl enable mattermost
+    systemctl enable certbot.timer
+    ufw allow OpenSSH
+    # Need full and not just https for certbot to work
+    ufw allow 'Nginx Full'
+    ufw delete allow 'Nginx HTTP'
+EOF
+
+# ufw enable might interrupt the ssh connection, which in turn fails because of
+# set -e.
+ssh -q root@${host} "yes | ufw enable" || true;
+
+set +x
+echo "Machine set up - please reboot it for all changes to take effect"
+echo "Run: ssh root@${host} \"reboot now\""
---
+++ notes.md
@@ -0,0 +1,91 @@
+# Server creation
+This is pretty straight forward in the Hetzner console: 
+
+1. Create server
+2. Good defaults
+    + Helsinki (no particular reason over Germany)
+    + Debian 12
+    + Dedicated vCPU
+        - This is for predictability and throughput. It is slightly more
+          expensive but not overly so.
+    + Otherwise cheapest at the time CCX13
+3. Add ssh public key for auth (~/.ssh/id_rsa.pub)
+4. Enable ipv4
+5. No extra storage volume
+    - These can be added later
+    - We don't need much storage for our workload
+    - Backups must be properly offsite anyway
+6. No firewall
+    - The firewall is an extra Hetzner concept, but I think the builtin
+      iptables should do fine for now
+    - At the time there is also no Hetzer firewall configured in the
+      subscription
+7. No backups
+    - Hetzner disk backups add 25% on price, but ideally we should be able to
+      re-init and re-deploy, which reduces the need for backups somewhat
+    - Regular backup should also be in-server configured
+8. No placement groups
+    - Not relevant for our needs
+9. No cloud config
+    - Cloud config is quite nice actually, but it is easier to track a proper
+      shell script for fresh-init which does the same thing.
+10. Name
+    - software-underground-1
+
+This is my public key:
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHRfPUZK61TxCbBYlyAdR4/X7ulBjYiBbTk+f8FzH77mZYEHzKAsuEPbKK+y3Z32zX5HcO5zoA0lHdJ+i5DeDsNxt9MXFaA4BYdnSNyP5RTVNFruvkYOJsRU0FFoBf1D4xu7y8H6RXbCSzk9jgUmdqbhyTLHJL3nC9D5iKB5gEvlI6JrKwQFfZPgs1rzUc/LYxT9i0jdcZbuhL6Docv2jqiYIZe0cNzJxlqO2uRDchmWwwr+buVm51wngLkgZRs3ciRx3YOMEN2Bji19otn9WR1DVwodwfkjjXu9MqrYlV9Vj+3GJW++smWPMtm1q7u6ymU8r2aPwHaJNa9rre7kE7QK2Ma5TAXmBd47+g0jnnKRLovIkFa3LDMj4wHzfHcxnclP7ka4v+L6Vx42wvvZHgpzJM0cXM0AO0gfRvImYM/r9++bBtRK4kQHx8cl3tB+tzoxbVEMTLExN3dQFd6UfJQ910A0PiA+RA2tGZP9yCclyBEJT0hj0CfIgZ3k8+qgX8gq+vToTyuJQUOvZphNLsiglcSPwlBAsnUZRnsV3AsubXo5VdUwHdibQO9/xBeswAWxzydubQ2lxYEyhDfCHJ6jPkBASm7qCL6wtd25AezwJfp8SyyaeMBMtsQuKZDyi1S+6iVzQ4oDyRFdEIJfB6vozH0Qjdk+TZ41DnIQHAyw== j@lambda.is
+
+# https and letsencrypt certificate
+**DO NOT TAKE THIS SECTION LIGHTLY**. If you are unsure what you are doing, stop
+and ask another maintainer for help. Do not keep the unencrypted certificates
+around for a long time. When extracting it to its install location, it is
+important that permissions and ownership is set properly.
+
+We need a certificate for the mattermost subdomain. We get one with certbot,
+which requires http/80 to be open, but is a one-time job (as the certificate
+should be used across deployments). It is therefore not a part of the
+automation, but the process is rendered here:
+
+```bash
+$ sudo ufw allow 80
+$ sudo certbot --nginx
+Successfully received certificate.
+Certificate is saved at: /etc/letsencrypt/live/mattermost.softwareunderground.org/fullchain.pem
+Key is saved at:         /etc/letsencrypt/live/mattermost.softwareunderground.org/privkey.pem
+This certificate expires on 2023-12-25.
+These files will be updated when the certificate renews.
+Certbot has set up a scheduled task to automatically renew this certificate in the background.
+
+$ sudo tar czf letsencrypt.tar.gz --owner 0 --group 0 -C /etc letsencrypt
+```
+
+I used my own email for emergency information and expiration alarms.
+
+The certificate is then encrypted with my publig gpg key and checked into the
+tree. An encrypted copy should be checked in for every maintainer using their
+public gpg keys, which means another maintainer needs to encrypt the archive
+for some other maintainer. Setting up gpg is out of scope for this document.
+
+To encrypt/decrypt:
+```bash
+$ gpg --encrypt --output cert/j-letsencrypt.tar.gz.gpg --recipient j@lambda.is letsencrypt.tar.gz
+$ gpg --decrypt -o letsencrypt.tar.gz j-letsencrypt.tar.gz
+```
+
+The naming `<user>-tarball.tar.gz.gpg` is conventional, but helps scripting.
+
+# Initialize the server
+run `init.sh mattermost.softwareunderground.org` (assuming DNS is set up), or
+just use the raw IP for the box. It must exist and have your ssh key in its
+authorized keys for root. This can be fixed in the hetzner (or provider)
+console.
+
+Some error checking is performed; should there be problems the only real
+recourse is to read the init.sh, which is written as a mostly straight forward
+shell script.
+
+# Health checks
+TODO
+
+# Backup
+TODO
