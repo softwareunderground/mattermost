36d3901fc7f7f8b642a5bdc3481524928f6cb2a3
Jørgen Kvalsvik <j@lambda.is>
2025-03-04 20:51:54 +0100

    Bump mattermost to version ESR 10.5.1
    The v9.11 series goes out of support in May 2025, so time to bump to the
    10.x extended support release series.

--- opt/mattermost/mattermost.scm
+++ opt/mattermost/mattermost.scm
@@ -6,14 +6,14 @@
 (define-public mattermost
   (package
     (name "mattermost")
-    (version "9.11.7")
+    (version "10.5.1")
     (source
       (origin
         (method url-fetch)
         (uri (string-append "https://releases.mattermost.com/" version
                             "/mattermost-" version "-linux-amd64.tar.gz"))
         (sha256
-          (base32 "0vakmnnryv5ds7723wajxr33qnhr9afyyf6y35azbrd84w0iy18p"))))
+          (base32 "1rkyrp9ds3by5mi9bj63a6vjcw086qgbksjl8gdbn9a5srb0bbwm"))))
     (build-system copy-build-system)
     (synopsis "mattermost server")
     (description "mattermost server")
