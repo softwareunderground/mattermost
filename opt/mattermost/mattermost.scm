7dbcc7d6be99f1c0c65f235228b1cbc34f52f250
Jørgen Kvalsvik <j@lambda.is>
2025-04-11 14:07:11 +0200

    Bump mattermost to 10.5.2

--- opt/mattermost/mattermost.scm
+++ opt/mattermost/mattermost.scm
@@ -6,14 +6,14 @@
 (define-public mattermost
   (package
     (name "mattermost")
-    (version "10.5.1")
+    (version "10.5.2")
     (source
       (origin
         (method url-fetch)
         (uri (string-append "https://releases.mattermost.com/" version
                             "/mattermost-" version "-linux-amd64.tar.gz"))
         (sha256
-          (base32 "1rkyrp9ds3by5mi9bj63a6vjcw086qgbksjl8gdbn9a5srb0bbwm"))))
+          (base32 "0g5h2jd92rh9vdfjghmx6ippgfkgnsrvy1s7rv8wgan0vyllmhwf"))))
     (build-system copy-build-system)
     (synopsis "mattermost server")
     (description "mattermost server")
