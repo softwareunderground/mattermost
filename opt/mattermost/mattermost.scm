2344d19a1723211ffa4fcd245528e9759290e91e
Jørgen Kvalsvik <j@lambda.is>
2026-06-02 22:40:14 +0200

    Replace mattermost with mostlymatter
    Starting from the v11.7 ESR series, the self-hosted mattermost is
    deliberately crippled with a 10k message limit built in. On top of
    being an obnoxious change this was a rather quiet change from
    mattermost upstream.
    
    Fortunately, there is a fork with this limit removed (or rather,
    bumped so high that it doesn't really matter). This is a satisfactory
    solution for now, pending a decision on whether (or when) we migrate.

--- opt/mattermost/mattermost.scm
+++ opt/mattermost/mattermost.scm
@@ -1,7 +1,26 @@
-(use-modules (guix packages)
+(use-modules (guix)
+             (guix packages)
              (guix download)
              (guix build-system copy)
              (guix licenses))
+
+(define-public mostlymatter
+  (package
+    (name "mostlymatter")
+    (version "11.7.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append
+              "https://packages.framasoft.org/projects/mostlymatter/mostlymatter-amd64-v"
+              version))
+        (sha256
+          (base32 "0kx1397g100k6gh36m7vz2k02fgwf3lxvga45pz4l7j2wfphkb5m"))))
+    (build-system copy-build-system)
+    (synopsis "mostlymatter server")
+    (description "mostlymatter server, a less hostile mattermost fork")
+    (home-page "https://framagit.org/framasoft/framateam/mostlymatter/")
+    (license expat)))
 
 (define-public mattermost
   (package
@@ -14,7 +33,17 @@
                             "/mattermost-" version "-linux-amd64.tar.gz"))
         (sha256
           (base32 "1i8332z8q6kjh5x3prw2asgifrbgrs36fdzh66dpi2wvxh2vkrqj"))))
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'install 'substitute-mostlymatter
+            (lambda* (#:key inputs #:allow-other-keys)
+              (let* ((mostlymatter-dir (assoc-ref inputs "mostlymatter"))
+                     (mostlymatter-bin (car (find-files mostlymatter-dir "mostlymatter"))))
+                (copy-file mostlymatter-bin "bin/mattermost")))))))
     (build-system copy-build-system)
+    (inputs (list mostlymatter))
     (synopsis "mattermost server")
     (description "mattermost server")
     (home-page "mattermost.com")
