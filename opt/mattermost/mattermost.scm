837a45c745987acdcece0ad6a3fecc63da09979c
Jørgen Kvalsvik <j@lambda.is>
2026-06-08 22:00:46 +0200

    Pass mostlymatter origin directly as input
    By not having this as a dedicated package the upgrade path is every so
    slightly easier, because now it will "inherit" (really, read the same
    var) the mattermost version.

--- opt/mattermost/mattermost.scm
+++ opt/mattermost/mattermost.scm
@@ -4,24 +4,6 @@
              (guix build-system copy)
              (guix licenses))
 
-(define-public mostlymatter
-  (package
-    (name "mostlymatter")
-    (version "11.7.1")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-              "https://packages.framasoft.org/projects/mostlymatter/mostlymatter-amd64-v"
-              version))
-        (sha256
-          (base32 "0kx1397g100k6gh36m7vz2k02fgwf3lxvga45pz4l7j2wfphkb5m"))))
-    (build-system copy-build-system)
-    (synopsis "mostlymatter server")
-    (description "mostlymatter server, a less hostile mattermost fork")
-    (home-page "https://framagit.org/framasoft/framateam/mostlymatter/")
-    (license expat)))
-
 (define-public mattermost
   (package
     (name "mattermost")
@@ -39,11 +21,19 @@
       #~(modify-phases %standard-phases
           (add-before 'install 'substitute-mostlymatter
             (lambda* (#:key inputs #:allow-other-keys)
-              (let* ((mostlymatter-dir (assoc-ref inputs "mostlymatter"))
-                     (mostlymatter-bin (car (find-files mostlymatter-dir "mostlymatter"))))
+              (let* ((mostlymatter-bin (assoc-ref inputs "mostlymatter")))
                 (copy-file mostlymatter-bin "bin/mattermost")))))))
     (build-system copy-build-system)
-    (inputs (list mostlymatter))
+    (inputs (list
+             (list "mostlymatter"
+              (origin
+                (method url-fetch)
+                (uri (string-append
+                      "https://packages.framasoft.org/projects/mostlymatter/mostlymatter-amd64-v"
+                      version))
+                (file-name "mostlymatter")
+                (sha256
+                 (base32 "0kx1397g100k6gh36m7vz2k02fgwf3lxvga45pz4l7j2wfphkb5m"))))))
     (synopsis "mattermost server")
     (description "mattermost server")
     (home-page "mattermost.com")
