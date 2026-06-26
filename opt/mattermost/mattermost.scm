978c31b399be4561a365d8ddb6a05e93e9bd20fc
Jørgen Kvalsvik <j@lambda.is>
2026-06-26 20:53:00 +0200

    Bump mattermost to 11.7.5

--- opt/mattermost/mattermost.scm
+++ opt/mattermost/mattermost.scm
@@ -7,14 +7,14 @@
 (define-public mattermost
   (package
     (name "mattermost")
-    (version "11.7.1")
+    (version "11.7.5")
     (source
       (origin
         (method url-fetch)
         (uri (string-append "https://releases.mattermost.com/" version
                             "/mattermost-" version "-linux-amd64.tar.gz"))
         (sha256
-          (base32 "1i8332z8q6kjh5x3prw2asgifrbgrs36fdzh66dpi2wvxh2vkrqj"))))
+          (base32 "1szwbhh317s0cfmmi8ahyia0fp1mnzrrwpzyl7r46zqrmnx9093s"))))
     (arguments
      (list
       #:phases
@@ -33,7 +33,7 @@
                       version))
                 (file-name "mostlymatter")
                 (sha256
-                 (base32 "0kx1397g100k6gh36m7vz2k02fgwf3lxvga45pz4l7j2wfphkb5m"))))))
+                 (base32 "1md1xff371khd3ivmc0i78nyz414zc131m6an7c04m10am99jg4b"))))))
     (synopsis "mattermost server")
     (description "mattermost server")
     (home-page "mattermost.com")
